46243b4ea0c2268de169dc70b0b4ae081e6a8f11
Sergio0703 <sercam0320@gmail.com>
2023-07-24 16:27:03 -0500

    Procedimiento almacenado

--- includes/db.php
+++ includes/db.php
@@ -5,12 +5,11 @@ $user = "root";
 $password = "";
 $database = "invent";
 
-
 $conexion = mysqli_connect($host, $user, $password, $database);
-if(!$conexion){
-echo "No se realizo la conexion a la basa de datos, el error fue:".
-mysqli_connect_error() ;
-
-
+if (!$conexion) {
+    echo "No se realizó la conexión a la base de datos, el error fue: " . mysqli_connect_error();
 }
+
+
+
 
